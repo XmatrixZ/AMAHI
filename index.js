edd00f27294bce7c5a85b78c7b3afc558022b75e
Richa-Modi <82664396+Richa-Modi@users.noreply.github.com>
2021-10-05 01:54:26 +0530

    Added validation using joi in login form but some bugs are there

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const Joi = require("joi")
+const bodyParser=require("body-parser");
 const path = require("path")
 const app = express()
 const port = 3000
+
 
 app.use(express.static('./'))
 app.use(express.urlencoded())
@@ -15,5 +18,21 @@ app.get("/", (req, res) => {
 // app.get("/clinc/",(req,res)=>{
 //     res.sendFile("./pages/clinc",{root:__dirname})
 // })
+app.post("/login",(req,res)=>{
+  console.log(req.body);
+  const loginShema=Joi.object({
+    email:Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    password:Joi.string().min(8).required()
+  }).unknown();
+  const {value,error}=loginShema.validate(req.body);
+  if(error){
+    console.log("Error",error);
+  }
+  else{
+    console.log(value);
+    res.send("Login completed succesfully!!")
+  }
+});
+
 
 app.listen(port,()=>console.log(`Listening on Port ${port}`))
