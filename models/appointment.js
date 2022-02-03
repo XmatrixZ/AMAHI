436aded640af7f75945c7cc19f018eda87d196e5
Shivansh Dhondiyal <shivanshdhondiyal7723@gmail.com>
2022-02-03 16:58:58 +0400

    Changes

--- models/appointment.js
+++ models/appointment.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Joi = require("joi");
 
 const appointmentSchema = new mongoose.Schema({
     name:{
@@ -37,4 +38,30 @@ const appointmentSchema = new mongoose.Schema({
 });
 
 const Appointment = mongoose.model('Appointment',appointmentSchema);
-module.exports = Appointment;
+
+class ValidationError extends Error {
+  constructor(message, fieldName) {
+    super(message);
+    this.fieldName = fieldName;
+  }
+}
+
+const validateAppointment = (Appointment) => {
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(30).required().messages({
+      "string.base": `"description" should be a type of 'text'`,
+      "string.min": '"title" has to be least {#limit} characters',
+      "string.max": '"title" has to be max {#limit} characters',
+      "any.required": `"title" is a required field`,
+    }),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    subject: Joi.string().max(50).required(),
+  })
+    .unknown()
+    .options({ abortEarly: false });
+  return schema.validate(Appointment);
+};
+
+module.exports = {Appointment,validateAppointment};
